d7caedf112e8772bf6d29f51ec8d3d9c559da38d
yaocheng <jsbyxyc@me.com>
2017-07-20 19:05:43 -0400

    original

---
+++ pebs/dumper.c
@@ -0,0 +1,123 @@
+/* Dump simple PEBS data from kernel driver */
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+typedef uint64_t u64;
+
+#include "simple-pebs.h"
+#include "dump-util.h"
+
+#define err(x) perror(x), exit(1)
+
+void dump_data(int cpunum, u64 *map, int num)
+{
+	int i;
+	/*
+	printf("dump %d\n", num);
+	for (i = 0; i < num; i++)
+		printf("%d: %lx\n", cpunum, map[i]);
+		*/
+}
+
+static void usage(void)
+{
+	fprintf(stderr, "Usage: dumper [-b]\n"
+		"-b binary dump\n");
+	exit(1);
+}
+
+int main(int ac, char **av)
+{
+	int size = get_size();
+	int ncpus = sysconf(_SC_NPROCESSORS_ONLN);
+	void *map[ncpus];
+	struct pollfd pfd[ncpus];
+	int opt;
+	bool binary = false;
+
+	while ((opt = getopt(ac, av, "b")) != -1) {
+		switch (opt) {
+		case 'b':
+			binary = true;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	int i;
+	for (i = 0; i < ncpus; i++)
+		open_cpu(&map[i], i, &pfd[i], size);
+	
+	int target = 7;
+	FILE *outfile;
+	outfile = fopen("tmp.ref","wb");
+	if(outfile == NULL){
+		exit(-1);
+	}
+	int _count = 0;
+	for(;_count<=1000;){
+		if(poll(pfd, ncpus, -1)<0)
+			perror("poll");
+		if(pfd[target].revents & POLLIN){
+			int len;
+
+			if(ioctl(pfd[target].fd, SIMPLE_PEBS_GET_OFFSET, &len) < 0){
+				perror("SIMPLE_PEBS_GET_OFFSET");
+				continue;
+			}
+			if(len>450000){
+				printf("%d\n",len);
+			if (binary)
+				fwrite(map[target], len,1,outfile);
+			else
+					dump_data(target, map[target], len / sizeof(u64));
+
+			if (ioctl(pfd[target].fd, SIMPLE_PEBS_RESET, 0) < 0) {
+					perror("SIMPLE_PEBS_RESET");
+					continue;
+			}	
+			_count++;
+			}
+		}
+	}
+	fclose(outfile);
+	/*	
+	for (;;) {
+		if (poll(pfd, ncpus, -1) < 0)
+			perror("poll");
+		for (i = 0; i < ncpus; i++) {
+			if (pfd[i].revents & POLLIN) {
+				int len;
+
+				if (ioctl(pfd[i].fd, SIMPLE_PEBS_GET_OFFSET, &len) < 0) {
+					perror("SIMPLE_PEBS_GET_OFFSET");
+					continue;
+				}
+
+				if (binary)
+					write(1, map[i], len);
+				else
+					dump_data(i, map[i], len / sizeof(u64));
+
+				if (ioctl(pfd[i].fd, SIMPLE_PEBS_RESET, 0) < 0) {
+					perror("SIMPLE_PEBS_RESET");
+					continue;
+				}
+			}
+		}
+	}
+*/
+	return 0;
+}
